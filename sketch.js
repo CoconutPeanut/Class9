b7a1930cf2318bee513ce8195109f87d9dc99380
CoconutPeanut <92638392+CoconutPeanut@users.noreply.github.com>
2021-10-16 15:42:28 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var test
+
+
+
+function setup() {
+  createCanvas(400,400);
+  test = createSprite (100, 100, 50, 50);
+  test.shapeColor = "teal";
+}
+
+function draw() 
+{
+  background("purple");
+  drawSprites ();
+
+}
+
+
+
+
